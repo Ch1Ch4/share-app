c4d09c33106393f09b039422c0e44daf176c8c44
Ch1Ch4\Marko <marko.radivojevic@gmail.com>
2018-04-18 20:35:36 +0200

    Login Logic

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -1,8 +1,7 @@
 <?php
   class Users extends Controller {
-
     public function __construct(){
-        $this->userModel = $this->model('User');
+      $this->userModel = $this->model('User');
     }
 
     public function register(){
@@ -29,10 +28,10 @@
         if(empty($data['email'])){
           $data['email_err'] = 'Pleae enter email';
         } else {
-            // Check email
-            if ($this->userModel->findUserByEmail($data['email'])) {
-                $data['email_err'] = 'Email is already taken';
-            }
+          // Check email
+          if($this->userModel->findUserByEmail($data['email'])){
+            $data['email_err'] = 'Email is already taken';
+          }
         }
 
         // Validate Name
@@ -59,16 +58,17 @@
         // Make sure errors are empty
         if(empty($data['email_err']) && empty($data['name_err']) && empty($data['password_err']) && empty($data['confirm_password_err'])){
           // Validated
-
-          // Hass Password
+          
+          // Hash Password
           $data['password'] = password_hash($data['password'], PASSWORD_DEFAULT);
 
           // Register User
-            if ($this->userModel->register($data)) {
-                redirect('users/login');
-            } else {
-                die('Something went wrong');
-            }
+          if($this->userModel->register($data)){
+            flash('register_success', 'You are registered and can log in');
+            redirect('users/login');
+          } else {
+            die('Something went wrong');
+          }
 
         } else {
           // Load view with errors
@@ -118,10 +118,28 @@
           $data['password_err'] = 'Please enter password';
         }
 
+        // Check for user/email
+        if($this->userModel->findUserByEmail($data['email'])){
+          // User found
+        } else {
+          // User not found
+          $data['email_err'] = 'No user found';
+        }
+
         // Make sure errors are empty
         if(empty($data['email_err']) && empty($data['password_err'])){
           // Validated
-          die('SUCCESS');
+          // Check and set logged in user
+          $loggedInUser = $this->userModel->login($data['email'], $data['password']);
+
+          if($loggedInUser){
+            // Create Session
+            $this->createUserSession($loggedInUser);
+          } else {
+            $data['password_err'] = 'Password incorrect';
+
+            $this->view('users/login', $data);
+          }
         } else {
           // Load view with errors
           $this->view('users/login', $data);
@@ -139,6 +157,29 @@
 
         // Load view
         $this->view('users/login', $data);
+      }
+    }
+
+    public function createUserSession($user){
+      $_SESSION['user_id'] = $user->id;
+      $_SESSION['user_email'] = $user->email;
+      $_SESSION['user_name'] = $user->name;
+      redirect('pages/index');
+    }
+
+    public function logout(){
+      unset($_SESSION['user_id']);
+      unset($_SESSION['user_email']);
+      unset($_SESSION['user_name']);
+      session_destroy();
+      redirect('users/login');
+    }
+
+    public function isLoggedIn(){
+      if(isset($_SESSION['user_id'])){
+        return true;
+      } else {
+        return false;
       }
     }
   }
